5f1c970cd7e7c554dcbcb995ee8d7acd2becb38a
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2024-04-26 19:42:33 +0600

    Auth Provider Added

---
+++ src/Providers/AuthProvider.jsx
@@ -0,0 +1,58 @@
+import { GithubAuthProvider, GoogleAuthProvider, createUserWithEmailAndPassword, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
+import { createContext, useEffect, useState } from "react";
+import auth from "../firebase/firebase.config";
+import PropTypes from 'prop-types';
+
+export const AuthContext = createContext(null);
+const googleProvider = new GoogleAuthProvider();
+const gitHubProvider = new GithubAuthProvider();
+const AuthProvider = ({ children }) => {
+
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+    const [update, setUpdate] = useState(true);
+
+    const createUser = (email, password) => {
+        setLoading(true);
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
+    const signInUser = (email, password) => {
+        setLoading(true);
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+    const signInWithGithub = () => {
+        setLoading(true);
+        return signInWithPopup(auth, gitHubProvider);
+    }
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentLoggedUser => {
+            setUser(currentLoggedUser);
+            setLoading(false);
+        })
+        return () => {
+            unsubscribe();
+        }
+    }, [user])
+
+    const authInfo = { user, update, setLoading, setUser, setUpdate, loading, signInWithGithub, signInWithGoogle, createUser, signInUser, logOut };
+
+    return (
+        <AuthContext.Provider value={authInfo}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+AuthProvider.propTypes = {
+    children: PropTypes.node,
+};
+
+export default AuthProvider;
