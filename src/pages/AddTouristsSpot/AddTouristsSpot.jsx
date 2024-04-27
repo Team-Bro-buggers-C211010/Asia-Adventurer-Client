09fbb1798c128e8bc5f094bcd34ea458dc5b5002
Team-Bro-buggers-C211010 <shovancse.iiuc.cp@gmail.com>
2024-04-28 02:49:30 +0600

    post the tourists spot data from client to server

--- src/pages/AddTouristsSpot/AddTouristsSpot.jsx
+++ src/pages/AddTouristsSpot/AddTouristsSpot.jsx
@@ -1,7 +1,44 @@
 const AddTouristsSpot = () => {
+    const handleAddTouristsSpot = e => {
+        e.preventDefault();
+
+        const form = e.target;
+        const userName = form.userName.value;
+        const userEmail = form.userEmail.value;
+        const spotName = form.spotName.value;
+        const country = form.country.value;
+        const location = form.location.value;
+        const description = form.description.value;
+        const season = form.season.value;
+        const travelTime = form.travelTime.value;
+        const avgCost = form.avgCost.value;
+        const visitors = form.visitors.value;
+        const photo = form.photo.value;
+
+        const newTouristsSpot = { userName, userEmail, spotName, country, location, description, season, travelTime, avgCost, visitors, photo };
+        console.log(newTouristsSpot);
+
+        // send data to the server
+        fetch("http://localhost:5000/all-tourists-spot", {
+            method: "POST",
+            headers: {
+                "content-type": "application/json"
+            },
+            body: JSON.stringify(newTouristsSpot)
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                if(data.insertedId){
+                    alert("New Tourists Spot Added !!!");
+                }
+                form.reset();
+            })
+
+    }
     return (
         <div className="container mx-auto mt-4 md:mt-12 px-1 md:px-0">
-            <form className="card-body border-2 border[#8e9281] rounded-xl bg-[#ccd7af] border-base-content md:w-2/3 flex mx-auto">
+            <form onSubmit={handleAddTouristsSpot} className="card-body border-2 border[#8e9281] rounded-xl bg-[#ccd7af] border-base-content md:w-2/3 flex mx-auto">
                 <h1 className="text-center text-xl mb-2 md:mb-4 md:text-3xl font-extrabold text-[#8e9281]">Add Your Tourists Spot</h1>
                 <div className="grid gap-3 grid-cols-1 md:grid-cols-2">
                     <div className="form-control">
@@ -56,7 +93,7 @@ const AddTouristsSpot = () => {
                         <label className="label font-bold">
                             <span className="label-text text-[#657a42]">Average Cost</span>
                         </label>
-                        <input type="text" name="avgcost" placeholder="Give the avg. cost" className="input input-bordered text-[#657a42] text-xs md:text-base" required />
+                        <input type="text" name="avgCost" placeholder="Give the avg. cost" className="input input-bordered text-[#657a42] text-xs md:text-base" required />
                     </div>
                     <div className="form-control">
                         <label className="label font-bold">
